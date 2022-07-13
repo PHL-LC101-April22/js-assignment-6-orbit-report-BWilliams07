0b0816c1ad484c8aae7d7bdbf90d13ea126095a9
BWilliams07 <104173554+BWilliams07@users.noreply.github.com>
2022-07-13 00:01:53 -0400

    Update orbit-list.component.ts

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -6,27 +6,36 @@ import { Satellite } from '../satellite';
   templateUrl: './orbit-list.component.html',
   styleUrls: ['./orbit-list.component.css']
 })
+
 export class OrbitListComponent implements OnInit {
+
   @Input() satellites: Satellite[];
+
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  objectKeys(satellite: object): string[] {
-    return Object.keys(satellite);
+  isEven(satellite: Satellite): boolean {
+	if ((this.satellites.indexOf(satellite)+1) % 2 === 1) {
+		return true;
+	} else {
+		return false;
+	}
   }
 
-  sort(column: string): void {
-    // array.sort modifies the array, sorting the items based on the given compare function
-    this.satellites.sort(function(a: Satellite, b: Satellite): number {
-       if(a[column] < b[column]) {
-          return -1;
-       } else if (a[column] > b[column]) {
-          return 1;
-       }
-       return 0;
-    });
- }
+	sort(column: string): void {
+		// array.sort modifies the array, sorting the items based on the given compare function
+		this.satellites.sort(function (a: Satellite, b: Satellite): number {
+			if (a[column] < b[column]) {
+				return -1;
+			} else if (a[column] > b[column]) {
+				return 1;
+			}
+			return 0;
+		});
+	}
+
 }
